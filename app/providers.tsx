155df1253fcd3c44a2bfb35ded07e49f45bebaab
Manoranjith  Shankar <admin@SMs-MacBook-Air.local>
2024-02-17 12:33:26 +0530

    added wallet provider rainbowkit

--- app/providers.tsx
+++ app/providers.tsx
@@ -6,6 +6,33 @@ import { useRouter } from 'next/navigation'
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
 
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+  zora,
+} from 'wagmi/chains';
+import {
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+const config = getDefaultConfig({
+  appName: 'projectX',
+  projectId: 'b4b96dd974332fe24cac9a91648749e7',
+  chains: [mainnet, polygon, optimism, arbitrum, base, zora],
+  ssr: true,
+});
+
 export interface ProvidersProps {
 	children: React.ReactNode;
 	themeProps?: ThemeProviderProps;
@@ -15,8 +42,14 @@ export function Providers({ children, themeProps }: ProvidersProps) {
   const router = useRouter();
 
 	return (
-		<NextUIProvider navigate={router.push}>
-			<NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
-		</NextUIProvider>
+		<WagmiProvider config={config}>
+		<QueryClientProvider client={queryClient}>
+		  <RainbowKitProvider>
+			<NextUIProvider navigate={router.push}>
+				<NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+			</NextUIProvider>
+		</RainbowKitProvider>
+		</QueryClientProvider>
+	  </WagmiProvider>
 	);
 }
