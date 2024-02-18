925e9017501847deb159c6a4b18761377b293a61
manoranjith shankar <ma.ran4073@gmail.com>
2024-02-18 13:33:04 +0530

    tested backend -test success

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,14 +1,15 @@
 'use client'
 
 import { Button } from '@nextui-org/button'
-import { ConnectButton } from '@rainbow-me/rainbowkit'
 import React from 'react'
 import toast from 'react-hot-toast'
+import testContract from '@/backend/deployedContracts/testContract.json'
 
 export default function Test() {
   const notify = () => {
     toast.success('working')
   }
+  console.log(testContract.networks[80001].address)
   return (
     <>
         <Button onClick={notify}>
