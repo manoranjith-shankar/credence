f28dc3847825c0238bc777878590cfcf1a3e35be
manoranjith shankar <ma.ran4073@gmail.com>
2024-02-22 19:39:34 +0530

    Add custom hooks for reading and writing to a contract

--- components/hooks/useContractRead.ts
+++ components/hooks/useContractRead.ts
@@ -3,6 +3,13 @@ import AppHandler from '@/backend/deployedContracts/AppHandler.json'
 const abi = AppHandler.abi
 const contractAddress = AppHandler.networks[80001].address
 
+/**
+ * Custom hook for reading data from a contract.
+ *
+ * @param functionName - The name of the contract function to call.
+ * @param args - Arguments to pass to the contract function.
+ * @returns An object containing the data, loading state, and error state.
+ */
 const useContractRead = (functionName, ...args) => {
   // Create a `useReadContract` instance with pre-filled details
   const { data, isLoading, error } = useReadContract({
--- components/hooks/useContractWrite.ts
+++ components/hooks/useContractWrite.ts
@@ -4,6 +4,13 @@ import AppHandler from '@/backend/deployedContracts/AppHandler.json';
 const abi = AppHandler.abi;
 const contractAddress = AppHandler.networks[80001].address;
 
+/**
+ * Custom hook for writing to a contract.
+ * 
+ * @param {string} functionName - The name of the function to call on the contract.
+ * @param {...any} args - Arguments to pass to the contract function.
+ * @returns {Object} - An object containing the data, pending status, and error of the contract write operation.
+ */
 const useContractWrite = (functionName, ...args) => {
     const { data, isPending, error } = useWriteContract({
       address: contractAddress,
